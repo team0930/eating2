777675a93f069e3612a5c211bb820c691f45cacc
vagrant <vagrant@localhost.localdomain>
2020-10-22 15:56:52 +0000

    restaurant validates

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -9,17 +9,17 @@ class Restaurant < ApplicationRecord
 	has_many :pictures, dependent: :destroy
   	accepts_attachments_for :pictures, attachment: :image
 
- #  	with_options presence: true do
-	#   validates :name
-	#   validates :introduction
-	#   validates :phone_number
-	#   validates :postal_code
-	#   validates :address
-	#   validates :area
-	#   validates :genre
-	#   validates :email
-	#   validates :pictures
-	# end
+  	with_options presence: true do
+	  validates :name
+	  validates :introduction
+	  validates :phone_number
+	  validates :postal_code
+	  validates :address
+	  validates :area
+	  validates :genre
+	  validates :email
+	  validates :pictures
+	end
 
 
 
